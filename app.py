ad0ce862c18ae0b1dbd97c8455c0b9ed59cd1902
Han Koh <hankkoh2@gmail.com>
2021-03-11 23:05:55 -0600

    added time

--- app.py
+++ app.py
@@ -1,8 +1,15 @@
 from flask import Flask
 from socket import gethostname
+from datetime import datetime
+
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello_world():
-    return 'Hello World! ' + gethostname()
+    return '''hostname: {hostname} |
+    time: {time}
+    '''.format(
+        hostname=gethostname(), 
+        time=datetime.now().strftime("%H:%M:%S")
+    )
